f1668f117e132a8dd09019fcb5e1129abffedeaf
Mkzay <80535194+Mkzay@users.noreply.github.com>
2025-01-04 18:10:16 +0100

    favicon

--- app/layout.jsx
+++ app/layout.jsx
@@ -11,6 +11,22 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </head>
       <body className="overflow-x-hidden">
         <AuthProvider>
           <Navbar />

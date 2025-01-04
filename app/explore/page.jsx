25cac06ab63e10c7d5971690d46b15d393d5b0c9
Mkzay <80535194+Mkzay@users.noreply.github.com>
2025-01-04 09:57:29 +0100

    ...

--- app/explore/page.jsx
+++ app/explore/page.jsx
@@ -1,21 +1,36 @@
 const Page = () => {
   return (
-    <section className="pt-20 px-5 flex flex-col gap-5 items-center justify-center">
+    <section className="pt-20 px-5 flex flex-col gap-10 items-center justify-center">
       {/* Header Section */}
       <div className="pt-10 flex flex-col items-center justify-center gap-2">
-        <h1 className="text-5xl font-serif">Make your E-Bookshelf!</h1>
+        <h1 className="text-5xl font-serif text-center">
+          Make your E-Bookshelf!
+        </h1>
         <p className="font-medium">Discover your next favorite book today!</p>
       </div>
 
       {/* Scrolling Section */}
       <div className="flex flex-col justify-center gap-5 w-full">
         <h2 className="text-xl font-bold">Top Rated</h2>
-        <div className="flex items-center justify-start bg-red-500 rounded-lg overflow-x-auto whitespace-nowrap p-2 gap-2 scrollbar-hide">
+        <div className="flex items-center justify-start bg-red-500 rounded-xl overflow-x-auto whitespace-nowrap p-1 gap-1 scrollbar-hide">
           {/* Book Cards */}
           {[...Array(10)].map((_, index) => (
             <div
               key={index}
-              className="w-44 h-36 bg-gray-300 rounded-lg flex-shrink-0"
+              className="w-44 h-36 bg-gray-300 rounded-xl flex-shrink-0"
+            ></div>
+          ))}
+        </div>
+      </div>
+
+      <div className="flex flex-col justify-center gap-5 w-full">
+        <h2 className="text-xl font-bold">Recommended</h2>
+        <div className="flex items-center justify-start rounded-lg overflow-x-auto whitespace-nowrap gap-2 scrollbar-hide">
+          {/* Book Cards */}
+          {[...Array(10)].map((_, index) => (
+            <div
+              key={index}
+              className="w-56 h-48 bg-gray-300 rounded-lg flex-shrink-0"
             ></div>
           ))}
         </div>
